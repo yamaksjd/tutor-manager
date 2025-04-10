1aacc0188c4529abd8a15822a0354e18ed5286a3
yamaksjd <luyanavunge@gmail.com>
2025-04-10 10:40:46 +0100

    first commit

---
+++ tutor_manager.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", () => {
+    const form = getElementById("sessionForm")
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const student = document.getElementById("student-selection").value
+        const date = document.getElementById("date").value
+        const duration = document.getElementById("duration").value
+
+        if(!student || !date || !duration) {
+            alert("Please fill out all of the requirements in the form")
+            return;
+        }
+
+    })
+    console.log("Form submitted! " + student +" "+ date + " " + duration)
+})
